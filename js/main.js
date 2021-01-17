072ca83389715807e91ef57af297bd2f92bd1215
Rastacrab <hummster@rambler.ru>
2021-01-18 00:58:52 +0300

    formating

--- js/main.js
+++ js/main.js
@@ -1,41 +1,40 @@
 (function () {
-  const header = document.querySelector('.header');
+  const header = document.querySelector(".header");
   window.onscroll = () => {
     if (window.pageYOffset > 50) {
-      header.classList.add('header_active');
+      header.classList.add("header_active");
     } else {
-      header.classList.remove('header_active');
+      header.classList.remove("header_active");
     }
   };
-}());
+})();
 
 // Burger handler
 (function () {
-  const burgerItem = document.querySelector('.burger');
-  const menu = document.querySelector('.header__nav');
-  const menuCloseItem = document.querySelector('.header__nav-close');
-  const menuLinks = document.querySelectorAll('.header__link');
-  burgerItem.addEventListener('click', () => {
-      menu.classList.add('header__nav_active');
+  const burgerItem = document.querySelector(".burger");
+  const menu = document.querySelector(".header__nav");
+  const menuCloseItem = document.querySelector(".header__nav-close");
+  const menuLinks = document.querySelectorAll(".header__link");
+  burgerItem.addEventListener("click", () => {
+    menu.classList.add("header__nav_active");
   });
-  menuCloseItem.addEventListener('click', () => {
-      menu.classList.remove('header__nav_active');
+  menuCloseItem.addEventListener("click", () => {
+    menu.classList.remove("header__nav_active");
   });
   if (window.innerWidth <= 767) {
-      for (let i = 0; i < menuLinks.length; i += 1) {
-          menuLinks[i].addEventListener('click', () => {
-              menu.classList.remove('header__nav_active');
-          });
-      }
+    for (let i = 0; i < menuLinks.length; i += 1) {
+      menuLinks[i].addEventListener("click", () => {
+        menu.classList.remove("header__nav_active");
+      });
+    }
   }
-}());
+})();
 
 //Scroll to anchors
 
 (function () {
-
   const smoothScroll = function (targetEl, duration) {
-    const headerElHeight = document.querySelector('.header').clientHeight;
+    const headerElHeight = document.querySelector(".header").clientHeight;
     let target = document.querySelector(targetEl);
     let targetPosition = target.getBoundingClientRect().top - headerElHeight;
     let startPosition = window.pageYOffset;
@@ -43,9 +42,9 @@
 
     const ease = function (t, b, c, d) {
       t /= d / 2;
-      if (t < 1) return c / 2 * t * t + b;
+      if (t < 1) return (c / 2) * t * t + b;
       t--;
-      return -c / 2 * (t * (t - 2) - 1) + b;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
     };
 
     const animation = function (currentTime) {
@@ -56,17 +55,16 @@
       if (timeElapsed < duration) requestAnimationFrame(animation);
     };
     requestAnimationFrame(animation);
-
   };
 
   const scrollTo = function () {
-    const links = document.querySelectorAll('.js-scroll');
-    links.forEach(each => {
-      each.addEventListener('click', function () {
-        const currentTarget = this.getAttribute('href');
+    const links = document.querySelectorAll(".js-scroll");
+    links.forEach((each) => {
+      each.addEventListener("click", function () {
+        const currentTarget = this.getAttribute("href");
         smoothScroll(currentTarget, 1000);
       });
     });
   };
   scrollTo();
-}());
+})();
